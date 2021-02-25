3270d574766e7969e55f17c63671abb9c6047a9e
D1scoDancer <apshulikov@edu.hse.ru>
2021-02-25 22:44:05 +0300

    Initial commit

---
+++ app/src/main/java/ru/alekssey7227/lifetime/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.alekssey7227.lifetime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
