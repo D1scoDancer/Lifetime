b13350422dc9231bab6140c52d3d3226a5c667b6
D1scoDancer <apshulikov@edu.hse.ru>
2021-02-27 10:48:37 +0300

    changed packages

--- app/src/main/java/ru/alekssey7227/lifetime/activities/MainActivity.java
+++ app/src/main/java/ru/alekssey7227/lifetime/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package ru.alekssey7227.lifetime;
+package ru.alekssey7227.lifetime.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import ru.alekssey7227.lifetime.R;
 
 public class MainActivity extends AppCompatActivity {
 
